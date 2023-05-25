7b8858534581a287a47cf1390065752d84b48f2e
Venktesh <111628674+VenkteshT@users.noreply.github.com>
2023-05-25 11:43:31 +0530

    added comments

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import "./dropdown.css";
 export default function Dropdown({ options }) {
+  
+//   state to hold the toggle value of option list
   const [close, setColose] = useState(false);
+  
+//  state which stores the selected option
   const [val, setVal] = useState("SELECT");
+  
+// mouse hover handler
   function handleMouseEnter() {
     setColose(false);
   }
+// option cliced handler
   function handleClick(e) {
     setColose(true);
     setVal(e.target.innerHTML);
   }
+  
   return (
     <div className="container">
       <h2>Should you use dropdown ?</h2>
