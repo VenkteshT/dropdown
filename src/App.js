5534f18fc40413a33a476ab36eb148a902f0ad56
Venktesh <111628674+VenkteshT@users.noreply.github.com>
2023-05-25 11:41:52 +0530

    added comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import "./App.css";
 import Dropdown from "./Dropdown";
 function App() {
+  
+//   state to store the options
   const [options, setOptions] = useState(["Yes", "probably not"]);
   return (
     <div className="App">
